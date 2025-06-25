335b6cd1a878c9d01e762ef79a2d39e65a501d07
Javohir <yoqubovjavohir331@gmail.com>
2025-06-25 18:36:40 +0500

    pizza

--- fergana/src/common/config/database.pravayder.ts
+++ fergana/src/common/config/database.pravayder.ts
@@ -16,7 +16,7 @@ import { KidsFaveEntity } from 'src/modules/products/kids-fave/entities/kids-fav
 
 export const databasePravader = TypeOrmModule.forRoot({
     type: 'postgres',
-    host: '172.17.0.1', // 172.17.0.1
+    host: 'host.docker.internal', // 172.17.0.1
     port: 5432,
     username: 'postgres',
     password: '23032006yj',
